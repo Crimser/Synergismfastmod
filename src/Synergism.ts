e5e05c9bbce97f1962bf04d558e4348f74b6dac4
Crimser <124676863+Crimser@users.noreply.github.com>
2024-09-20 10:59:14 -0700

    Update Synergism.ts

--- src/Synergism.ts
+++ src/Synergism.ts
@@ -5863,7 +5863,7 @@ const tick = () => {
     // compute at max delta ms to avoid negative delta
     dtEffective = Math.min(delta, dtEffective)
     // run tack and record timings
-    tack(dtEffective / 1000)
+    tack((dtEffective / 1000) *1000)
     lastUpdate += dtEffective
     delta -= dtEffective
   }
